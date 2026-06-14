cd1a0d35d4b819503bcec71cd668bb4995915d13
NegativePlayeer <108997804+NegativePlayeer@users.noreply.github.com>
2026-06-14 23:09:31 +0200

    chore: add cors_origin to render

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app = FastAPI(title="Elden Ring Ultimate Build Crafter API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["https://ultimate-elden-ring-dashboard.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
